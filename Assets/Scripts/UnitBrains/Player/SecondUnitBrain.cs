5b69552043f3b5fa8f250afbfd4b5caeecdd6c26
ViT BaR <barantsev.vitalik@mail.ru>
2025-09-06 13:24:49 +0300

    Домашнее задание №5
    Изменение атаки каждой цели поблизости

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,6 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using Model;
+using Model.Runtime;
 using Model.Runtime.Projectiles;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using Utilities;
 
 namespace UnitBrains.Player
 {
@@ -12,13 +17,12 @@ namespace UnitBrains.Player
         private float _temperature = 0f;
         private float _cooldownTime = 0f;
         private bool _overheated;
-        
+
+        private List<Vector2Int> _targetsOutOfRange = new List<Vector2Int>();
+
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
             float overheatTemperature = OverheatTemperature;
-            ///////////////////////////////////////
-            // Homework 1.3 (1st block, 3rd module)
-            ///////////////////////////////////////    
             int temperature = GetTemperature();
 
             if (temperature >= overheatTemperature)
@@ -26,54 +30,82 @@ namespace UnitBrains.Player
                 return;
             }
 
-            for(int i = 0;  i <=temperature; ++i)
+            for (int i = 0; i <= temperature; ++i)
             {
                 var projectile = CreateProjectile(forTarget);
                 AddProjectileToList(projectile, intoList);
             }
 
             IncreaseTemperature();
-            ///////////////////////////////////////
         }
 
         public override Vector2Int GetNextStep()
         {
-            return base.GetNextStep();
+            if (_currentMovementTarget == null) return unit.Pos;
+
+            return unit.Pos.CalcNextStepTowards(_currentMovementTarget.Value);
         }
+
+        private Vector2Int? _currentMovementTarget;
+        private List<Vector2Int> _reachableTargets = new List<Vector2Int>();
+        private static int _countTargets = 0;
+        private int _numberUnit;
+        private const int _maxTargetsToChoose = 3;
 
         protected override List<Vector2Int> SelectTargets()
         {
-            ///////////////////////////////////////
-            // Homework 1.4 (1st block, 4rd module)
-            ///////////////////////////////////////
-            List<Vector2Int> result = GetReachableTargets();
-
-            if (result.Count != 0)
+            if (_numberUnit == 0)
             {
-                Vector2Int minTarget = result[0];
-                float minDistance = DistanceToOwnBase(minTarget);
-
-                for (int i = 1; i < result.Count; i++)
-                {
-                    float currentDistance = DistanceToOwnBase(result[i]);
-                    if (currentDistance < minDistance)
-                    {
-                        minDistance = currentDistance;
-                        minTarget = result[i];
-                    }
-                }
-                return new List<Vector2Int> { minTarget };
+                _numberUnit = _countTargets;
+                _countTargets++;
             }
-            else return new List<Vector2Int>();
-            ///////////////////////////////////////
+
+            _targetsOutOfRange.Clear();
+            List<Vector2Int> result = new List<Vector2Int>();
+
+            // ---------------------ДЗ 5 (2.3)---------------------
+            // Очистка списка. Запись всех целей в очищенный список.
+            _reachableTargets.Clear();
+            foreach (var target in GetAllTargets().ToList())
+            {
+                _reachableTargets.Add(target);
+            }
+
+            if (_reachableTargets.Count > 0)
+            {
+                // Сортировка целей по дистанции
+                SortByDistanceToOwnBase(_reachableTargets);
+                int targetIndex = _numberUnit % _maxTargetsToChoose;
+
+                if (targetIndex >= _reachableTargets.Count) targetIndex = _reachableTargets.Count - 1; ;
+
+                Vector2Int selectedTarget = _reachableTargets[targetIndex];
+
+                if (IsTargetInRange(selectedTarget))
+                {
+                    result.Add(selectedTarget);
+                    _currentMovementTarget = null;
+                }
+                else
+                {
+                    _currentMovementTarget = selectedTarget;
+                    _targetsOutOfRange.Add(selectedTarget);
+                }
+            }
+            else
+            {
+                _currentMovementTarget = runtimeModel.RoMap.Bases[RuntimeModel.BotPlayerId];
+            }
+
+            return result;
         }
 
         public override void Update(float deltaTime, float time)
         {
             if (_overheated)
-            {              
+            {
                 _cooldownTime += Time.deltaTime;
-                float t = _cooldownTime / (OverheatCooldown/10);
+                float t = _cooldownTime / (OverheatCooldown / 10);
                 _temperature = Mathf.Lerp(OverheatTemperature, 0, t);
                 if (t >= 1)
                 {
@@ -85,7 +117,7 @@ namespace UnitBrains.Player
 
         private int GetTemperature()
         {
-            if(_overheated) return (int) OverheatTemperature;
+            if (_overheated) return (int)OverheatTemperature;
             else return (int)_temperature;
         }
 
